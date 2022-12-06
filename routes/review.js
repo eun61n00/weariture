09cbef452f08116687d384f456713678ed8eca79
eunbin <eun61n@gmail.com>
2022-12-06 12:49:20 +0900

    [FEAT] Writing Reviews

--- routes/review.js
+++ routes/review.js
@@ -4,8 +4,33 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
 	const review = await Review.findAll({});
-	console.log(review);
 	res.render("review", { review });
 });
+
+router.get("/write", async(req, res) => {
+	const review = await Review.findAll({});
+	res.render("review_write", { review });
+})
+
+router.post("/write", async(req, res) => {
+	let review = await Review;
+	review.create({
+		pro_name: req.body.pro_name,
+		tit: req.body.review_content,
+		cate: req.body.cate,
+		cus_name: req.body.cus_name,
+		re_date: 'test',
+		view: 0,
+		rec: 0,
+		rate: req.body.rate
+	})
+	.then(function(createReviewRecord) {
+		// TODO review 페이지 다시 렌더링
+		review = Review.findAll({});
+		console.log(review);
+		res.render('review', { review });
+	});
+
+})
 
 module.exports = router;
